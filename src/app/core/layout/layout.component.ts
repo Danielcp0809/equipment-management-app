68f50b839d80afd7f9d3d9bb5381c6eebd97eff3
Daniel Cordova UDT <jdany0809@hotmail.com>
2022-11-04 01:14:05 -0500

    Menu options added

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  menuItems: any[] = [
+    {
+      title: 'Users',
+      path: 'users',
+    },
+    {
+      title: 'Equipment',
+      path: 'equipment',
+    },
+  ];
 
-  constructor() { }
+  route: string = '';
+
+  constructor(public router: Router) {}
 
   ngOnInit(): void {
+    this.route = this.router.url;
   }
 
+  onClickItem(path: string) {
+    this.router.navigate([path]);
+    this.route = '/' + path;
+  }
 }
