fa81e678332130c768d33d58608a4e231e9aceb6
Daniel Cordova UDT <jdany0809@hotmail.com>
2022-11-04 01:13:08 -0500

    MatIcon and MatCard modules added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    MatIconModule
-  ],
-  exports:[
-    MatIconModule
-  ]
+  imports: [CommonModule, MatIconModule, MatCardModule],
+  exports: [MatIconModule, MatCardModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
