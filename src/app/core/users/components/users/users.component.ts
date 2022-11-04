b756bbb4de818246943c6973fa82b0900405102e
Daniel Cordova UDT <jdany0809@hotmail.com>
2022-11-04 13:26:31 -0500

    new user form added to the users view

--- src/app/core/users/components/users/users.component.ts
+++ src/app/core/users/components/users/users.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { newUserModel } from 'src/app/shared/models/newUser.model';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  users: any[] = [1, 1, 1, 1, 1, 1, 11, 1, 1, 1];
+  newUserForm!: FormGroup;
 
-  users:any[]=[1,1,1,1,1,1,11,1,1,1]
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
   }
 
+  ngOnInit(): void {}
+
+  buildForm() {
+    this.newUserForm = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+    });
+  }
+
+  openModal(id: string) {
+    document.querySelector('#' + id)?.classList.add('md-show');
+  }
+
+  onNewUserSave(newUser: newUserModel) {
+    console.log(newUser);
+  }
 }
