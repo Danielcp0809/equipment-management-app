13a098462375a7402a8fab4df172cc1a540e0c34
Daniel Cordova UDT <jdany0809@hotmail.com>
2022-11-06 00:58:48 -0500

    get all user endpoint implemented into users view

--- src/app/core/users/components/users/users.component.ts
+++ src/app/core/users/components/users/users.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user/user.service';
 import { newUserModel } from 'src/app/shared/models/newUser.model';
+import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-users',
@@ -8,14 +10,23 @@ import { newUserModel } from 'src/app/shared/models/newUser.model';
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-  users: any[] = [1, 1, 1, 1, 1, 1, 11, 1, 1, 1];
+  users: User[] = [];
   newUserForm!: FormGroup;
+  isEdit: boolean = false;
+  userToEdit!: User;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) {
     this.buildForm();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe((data: any) => {
+      this.users = data
+    })
+  }
 
   buildForm() {
     this.newUserForm = this.formBuilder.group({
@@ -30,6 +41,53 @@ export class UsersComponent implements OnInit {
   }
 
   onNewUserSave(newUser: newUserModel) {
-    console.log(newUser);
+    this.userService.createUser(newUser).subscribe((data: User) => {
+      this.users = [...this.users, data]
+      this.closeModal('new-user');
+      this.newUserForm.reset();
+    })
+  }
+
+  closeModal(id: string) {
+    document.querySelector('#' + id)?.classList.remove('md-show');
+    this.newUserForm.reset();
+  }
+
+  onUserDelete(id: string) {
+    try {
+      this.userService.deleteUser(id).subscribe((res) => {
+        this.users = this.users.filter((user:User) => user.id !== id)
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
+  onUserEdit(user: User){
+    this.userToEdit = user;
+    this.isEdit = true;
+    const userInfo: newUserModel = {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email
+    }
+    this.newUserForm.reset(userInfo)
+    this.openModal('new-user')
+  }
+
+  onUserSave(user: newUserModel){
+    this.userService.updateUser(this.userToEdit.id, user).subscribe((res) => {
+      this.users = this.users.map((oldUser:User) => {
+        if(oldUser.id === this.userToEdit.id){
+          oldUser.firstName = user.firstName;
+          oldUser.lastName = user.lastName;
+          oldUser.email = user.email;
+          oldUser.active = user.active ? user.active : this.userToEdit.active;
+        }
+        return oldUser;
+      });
+      this.closeModal('new-user');
+      this.isEdit = false;
+    })
   }
 }
