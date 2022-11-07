8e51fc2267eae6ecbcc3138f325615a95ec3adae
Daniel Cordova <jdany0809@hotmail.com>
2022-11-07 00:47:16 -0500

    loading spinner added to user and equipment view

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatIconModule, MatCardModule],
-  exports: [MatIconModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+  ],
+  exports: [MatIconModule, MatCardModule, MatProgressSpinnerModule],
 })
 export class MaterialModule {}
